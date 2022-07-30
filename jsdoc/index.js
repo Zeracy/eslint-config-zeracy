96b088ecc4164d009ff8d5b534f8b98603dd9760
Zeracy <joaoddvieira@gmail.com>
2022-07-30 18:03:34 +0100

    Split the required from jsdoc into it's own config

--- jsdoc/index.js
+++ jsdoc/index.js
@@ -13,14 +13,6 @@ module.exports = {
         },
     },
     rules: {
-        'require-jsdoc': [1, {
-            require: {
-                FunctionDeclaration: true,
-                MethodDefinition: true,
-                ClassDeclaration: false,
-                ArrowFunctionExpression: true,
-            },
-        }],
         'valid-jsdoc': [1, {
             prefer: {
                 arg: 'param',
